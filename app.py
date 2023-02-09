876fe5ece1935a729cbbe6f1e6d07cf16bcf6e88
codeteme <codeteme@gmail.com>
2023-02-09 18:05:14 +0300

    Display error & success message on the web app UI

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # You can run the streamlit as follows: streamlit run ui/app.py
 
 import streamlit as st
-import pyktok as pyk
+from src.pyktok import pyktok as pyk
 
 
 st.set_page_config(page_title="Pyktok Streamlit app")
@@ -47,46 +47,54 @@ def is_tiktok_url(url):
 if selected_key == 'download_single_video':
     single_tt_vid_url = st.text_input('Enter the URL of the video: ')
     if not single_tt_vid_url:
-        st.write("Error: Empty URL")
+        st.error("Error: Empty URL")
     elif not is_tiktok_url(single_tt_vid_url):
-        st.write("Error: Not a valid TikTok URL")
+        st.error("Error: Not a valid TikTok URL")
     else:
         pyk.save_tiktok(single_tt_vid_url, True, 'video_data.csv',  browser_name)
+        # There is no need to check for the return value from the line above 
+        # because the previous statements ensure that only valid arguments 
+        # are passed to the function
+        st.success('Download Completed')
 
+        
 
 elif selected_key == 'download_metadata':
     multi_tt_urls = st.text_input('Enter the URLs of the videos separated by a comma: ', )
     if not multi_tt_urls:
-        st.write("Error: Empty URL")
-
+        st.error("Error: Empty URL")
     else:
         multi_tt_url_list = [url.strip() for url in multi_tt_urls.split(',') if is_tiktok_url(url.strip())]
         if not multi_tt_url_list:
-            st.write("Error: No valid TikTok URLs found")
+            st.error("Error: No valid TikTok URLs found")
         else:
             include_video_download =  st.radio( "Do you want to download the videos too?", ('No', 'Yes'), horizontal=True)
             pyk.save_tiktok_multi_urls(multi_tt_url_list, include_video_download == 'Yes', 'tiktok_data.csv', 1)
+            st.success('Downloads Completed')
 
 elif selected_key in ('download_user_page', 'download_hashtag_page', 'download_music_page'):
     tt_multipage_url = st.text_input("Enter the URL:")
     if not tt_multipage_url:
-        st.write("Error: Empty URL")
+        st.error("Error: Empty URL")
     elif not is_tiktok_url(tt_multipage_url):
-        st.write("Error: Not a valid TikTok URL")
+        st.error("Error: Not a valid TikTok URL")
     else:
         save_video = st.radio("Do you want to download the video?", ('No', 'Yes'))
         pyk.save_tiktok_multi_page(tt_multipage_url, save_video=='Yes', save_metadata=True, browser_name=browser_name)
+        st.success('Download completed')
 
 
 elif selected_key in ('download_comments', 'download_comments_multiple_videos'):
     tt_visible_comments_str = st.text_input("Enter the URLs of the videos separated by a comma:")
     if not tt_visible_comments_str:
-        st.write("Error: Empty URL")
+        st.error("Error: Empty URL")
     else:
         tt_visible_comments_list = [url.strip() for url in tt_visible_comments_str.split(',') if is_tiktok_url(url.strip())]
         if not tt_visible_comments_list:
-            st.write("Error: No valid TikTok URLs found")
-
-
-
-
+            st.error("Error: No valid TikTok URLs found")
+        else:
+            for v in tt_visible_comments_list:
+                if pyk.save_visible_comments(v) is None: 
+                    st.error('No comments found')
+                else: 
+                    st.success('Download completed')
